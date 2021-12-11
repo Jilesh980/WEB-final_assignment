9ae3aca18fdd6e5c92224f756873bf6f9d5baac0
Jilesh980 <88798168+Jilesh980@users.noreply.github.com>
2021-12-10 19:56:22 -0500

    Add files via upload

---
+++ info.js
@@ -0,0 +1,12 @@
+
+
+function matchPassword() {  
+    var pw1 = document.getElementById("password");  
+    var pw2 = document.getElementById("password2");  
+    if(password != password2)  
+    {   
+      alert("Passwords did not match");  
+    } else {  
+      alert("Password created successfully");  
+    }  
+  }  
